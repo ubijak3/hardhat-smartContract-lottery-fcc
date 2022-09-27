e3aa25e87f41a6ede8fed7d6d6fc549835ce42f9
Marcin <marcink3a@gmail.com>
2022-09-27 14:43:04 +0200

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,12 +6,10 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-const GOERLI_RPC_URL =
-  process.env.GOERLI_RPC_URL || "https://goerli.infura.io/v3/174af4826d9444c59c5fad24f128e4c5"
-const PRIVATE_KEY =
-  process.env.PRIVATE_KEY || "932f5c6bfdd5575c48ce7a4a57e18fbe2d9f6a56e4d1bad5f4f89c33a0cd206d"
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "T3BS3Y1DFTNJCQH9SPXIIYW8X152BQRH5E"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
   defaultNetwork: "hardhat",
